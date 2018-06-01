c2e6834a8f9e8fb8f5a9bb70e68aed32431e3970
Oscar Vasquez <oavasquez01@gmail.com>
2018-06-01 16:13:09 -0600

    avances de calendario

--- src/App.js
+++ src/App.js
@@ -1,124 +1,104 @@
 import React, { Component } from 'react';
-import { Button } from 'primereact/components/button/Button';
 
-import { Growl } from 'primereact/components/growl/Growl';
+import SideNav, {NavIcon, NavText } from 'react-sidenav';
 
+
+
+
+//import logo from './logo.png';
 import './App.css';
 import 'primereact/resources/themes/omega/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'font-awesome/css/font-awesome.css';
+import 'fullcalendar/dist/fullcalendar.css';
+
+import 'fullcalendar/dist/fullcalendar.js';
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = { count: 0 };
-    this.increment = this.increment.bind(this);
-    this.showSuccess = this.showSuccess.bind(this);
-    this.showInfo = this.showInfo.bind(this);
-    this.showWarn = this.showWarn.bind(this);
-    this.showError = this.showError.bind(this);
-    this.showMultiple = this.showMultiple.bind(this);
-    this.showSticky = this.showSticky.bind(this);
-    this.showCustom = this.showCustom.bind(this);
-    this.clear = this.clear.bind(this);
-  }
+    constructor() {
+        super();
 
 
-  showSuccess() {
-    this.growl.show({ severity: 'success', summary: 'Success Message', detail: 'Order submitted' });
-}
+        this.state = {
+            value1: 5,
+            value2: 10,
+            visibleLeft: false,
+            visibleRight: false,
+            visibleTop: false,
+            visibleBottom: false,
+            visibleFullScreen: false,
+            isOpen: false
+        };
 
-showInfo() {
-    this.growl.show({ severity: 'info', summary: 'Info Message', detail: 'PrimeReact rocks' });
-}
 
-showWarn() {
-    this.growl.show({ severity: 'warn', summary: 'Warn Message', detail: 'There are unsaved changes' });
-}
 
-showError() {
-    this.growl.show({ severity: 'error', summary: 'Error Message', detail: 'Validation failed' });
-}
+    }
 
-showSticky() {
-    this.growl.show({ severity: 'info', summary: 'Sticky Message', detail: 'You need to close Me', sticky: true });
-}
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 
-showCustom() {
-    let summary = <span><i className="fa fa-check" /> <strong>PrimeReact</strong></span>;
-    let detail = <img alt="PrimeReact" src="showcase/resources/images/primereact-logo.png" width="250px"/> 
 
-    this.growl.show({ severity: 'info', summary: summary, detail: detail, sticky: true });
-}
+    render() {
 
-showMultiple() {
-    this.growl.show([
-        {severity:'info', summary:'Message 1', detail:'PrimeReact rocks'},
-        {severity:'info', summary:'Message 2', detail:'PrimeReact rocks'},
-        {severity:'info', summary:'Message 3', detail:'PrimeFaces rocks'}
-    ]);
-}
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Options
+                  </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Option 1
+                    </DropdownItem>
+                                    <DropdownItem>
+                                        Option 2
+                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        Reset
+                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
 
-clear() {
-    this.growl.clear();
-}
-
-  increment() {
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  render() {
-    return (
-      <div>
-                <div className="content-section introduction growl-demo">
-                    <div className="feature-intro">
-                        <h1>Growl</h1>
-                        <p>Growl is used to display messages in an overlay.</p>
-                    </div>
-                </div>
-
-                <div className="content-section implementation ui-fluid">
-                    <Growl ref={(el) => { this.growl = el; }}></Growl>
-
-                    <h3>Severities</h3>
-                    <div className="ui-g">
-                        <div className="ui-g-12 ui-md-3">
-                            <Button onClick={this.showSuccess} label="Success" className="ui-button-success" />
-                        </div>
-                        <div className="ui-g-12 ui-md-3">
-                            <Button onClick={this.showInfo} label="Info" className="ui-button-info" />
-                        </div>
-                        <div className="ui-g-12 ui-md-3">
-                            <Button onClick={this.showWarn} label="Warn" className="ui-button-warning" />
-                        </div>
-                        <div className="ui-g-12 ui-md-3">
-                            <Button onClick={this.showError} label="Error" className="ui-button-danger" />
-                        </div>
-                    </div>
-                    
-                    <h3>Options</h3>
-                    <div className="ui-g">
-                        <div className="ui-g-12 ui-md-4">
-                            <Button onClick={this.showMultiple} label="Multiple" className="ui-button-warning" />
-                        </div>
-                        <div className="ui-g-12 ui-md-4">
-                            <Button onClick={this.showSticky} label="Sticky" />
-                        </div>
-                        <div className="ui-g-12 ui-md-4">
-                            <Button onClick={this.showCustom} label="Custom" className="ui-button-success" />
-                        </div>
-                    </div>
-                    
-                    <h3>Remove All</h3>
-                    <Button onClick={this.clear} label="Clear" style={{width: 'auto', marginLeft: '.5em'}}/>
-                </div>
                 
-            
             </div>
 
-    );
-  }
+
+
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello, world!'
+);
+
 
 ReactDOM.render(
   <App />,
